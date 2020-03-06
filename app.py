3cad0b09c1ac1684e1a1f1127c5c21d0eaa20172
neeltechie <neeltechie@gmail.com>
2020-03-07 03:49:00 +0530

    app with no db

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restplus import Resource, Api
+import time
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route("/index")
+class HelloWorld(Resource):
+    def get(self):
+        return {"time": time.asctime(time.localtime())}, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
